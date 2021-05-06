d452840489e85d1f9d0c1c0ac04dcf3841eb5288
Sofia-17 <66007184+Sofia-17@users.noreply.github.com>
2021-05-06 09:07:33 +0300

    Add files via upload

---
+++ CFunctions.cpp
@@ -0,0 +1,397 @@
+/*
+ * CFunctions.cpp
+ * 8. Определить класс CVector для работы с вектором вещественных
+ * чисел. Длина вектора задается в конструкторе класса. В классе
+ * должны быть определены необходимые конструкторы, деструктор, операторы сло-
+ * жения, вычитания, скалярного умножения.
+ * Написать функцию и конструктор сохранения переменной данного
+ * типа в файл и загрузки из файла.
+ * В отдельном файле должен быть написан тест на данный класс.
+ *     
+ */
+ 
+#include<iostream>
+#include<fstream>
+#include <sstream>
+#include"CVektor.h"
+//size_t glob_n;
+//////////////////CLASS METHODS///////////////////////////
+//////CVektor
+void CVektor::CopyOnly(const CVektor &v)
+    {
+     if(this!=&v)
+     {memcpy(ptrArr=new float[n=v.n], v.ptrArr, v.n*sizeof(float)); }
+    }
+void CVektor::GetVektor()        //Задать вектор
+    {
+        //cout << "Введите размерность вектора: "; cin >> n;
+        for (size_t i=0; i < n; i++ )
+        {
+            cout << "Введите элемент вектора № " << i << ": ";
+            cin >> ptrArr[i];
+        }
+    }
+void CVektor::ShowVektor()       //Показать вектор
+    {
+        cout << "Вектор {";
+        for (size_t i=0; i < n; i++)
+        {  
+         if(i<n-1)
+            cout << ptrArr[i] << ", ";
+         else
+            cout << ptrArr[i];
+        }    
+        cout << "}\n";
+    }
+ 
+CVektor& CVektor::operator=(const CVektor& v)
+    {
+        if(this!=&v) 
+        {if(ptrArr!=NULL)delete [] ptrArr;
+         CopyOnly(v);}
+        return *this;
+    }
+/*CVektor& CVektor::operator=( CVektor0 &b)
+{ 
+if(this!=&b )
+{memcpy(ptrArr,b.getPtrArr(),b.getN()*sizeof(float)); n=b.getN();} 
+return *this;
+}*/
+/*static */
+CVektor** CVektor::Input(const char *fn)
+{
+
+CVektor **v=new CVektor*[11]; 
+memset(v,0,sizeof(CVektor*)*11);
+//CVektor **v=NULL;
+std::ifstream finput; 
+//char mystring[100];
+   finput.open(fn, ios::in);
+   if (!finput) {
+    cout << "File error - can't open to read data!";
+    cin.sync(); cin.get(); throw -1;
+   }
+   
+       bool b; float k=0; int n=0;
+       char s[15]; string str;
+     //for(int i=0;!(finput.eof());i++)
+     //if ((fgets(mystring[i], 100, f) != NULL) ) // считать символы из файла
+      size_t i=0;
+      //LC:
+      while(getline(finput,str))
+      {stringstream ss(str);
+      //finput.getline(mystring, 100);
+       //вырезать имя файла и присвоить его
+      ss >> b;
+      //if(b==1) {goto lk;}
+      ss >> s;
+      //cout<<"\ns="<<s<<endl;
+      ss >> n;
+      //cout<<"n="<<n<<endl;
+      if(b==0) { v[i]=new CVektor0(n); //maybe need to kill vec
+                 v[i]->setText(s);
+                 }
+      else if(b==1) {v[i]=new CVektor1(n); //maybe need to kill vec
+                     v[i]->setText(s);
+                    }
+      //cout<<"xxx";
+      for(int j=0;(ss>>k)&&(j<n);j++)
+      {//(*v[i])[j]=k; 
+      (*v[i]).setPos(j,k);
+      //cout<<(*v[i])[j]<<" ";
+      }
+      //cout<<endl;
+      //if(finput.eof()){break;}
+      i++;
+       }   
+       //REMEMBER IT!
+       glob_n=i;
+   
+     //  lk:
+    finput.close();
+     //fclose (f);
+
+return v;
+}
+//////CVektor0    
+int CVektor0::output(const char *FileName)
+{
+   
+   if(FileName){
+   std::ofstream foutput;
+   if (!foutput) {
+    cout << "File error - can't open to write data!";
+    cin.sync(); cin.get(); throw -1;
+   }
+   foutput.open(OutFile, ios::out | ios::app);
+       for(size_t i=0;i<n;i++){foutput<<ptrArr[i]<<" ";} 
+       foutput << endl;
+       //cout<<*this;
+          
+     //  lk:
+    foutput.close();
+  }
+     return 0;
+
+}
+    
+/////CVektor1
+int CVektor1::output(const char *FileName)
+{
+  // char mystring[100];
+ 
+   //if (f == NULL) perror("Ошибка открытия файла");
+   //else
+   if(FileName){
+  std::ofstream foutput;
+   if (!foutput) {
+    cout << "File error - can't open to write data!";
+    cin.sync(); cin.get(); throw -1;
+   }
+  
+   foutput.open(OutFile, ios::out | ios::app);
+   {
+       foutput<<"{\n";
+       for(size_t i=0;i<n;i++){foutput<<ptrArr[i]<<endl;} 
+       foutput<<"}\n";
+        // cout<<*this;
+         //foutput<<this;
+  
+          
+    foutput.close();
+  }
+}
+return 0;
+}
+    
+    
+    
+/////////OPERATORS///////////////
+CVektor& operator+(const CVektor &lhs, const CVektor &rhs)
+{
+ if(lhs.getN()==rhs.getN())
+     {
+     //const CVektor& w=rhs; const CVektor& u=lhs; 
+     const CVektor& tmpl=lhs; const CVektor& tmpr=rhs;
+     CVektor& l=const_cast<CVektor&>(tmpl);//=const_cast<CVektor&>(w);
+     CVektor& r=const_cast<CVektor&>(tmpr);
+     CVektor& res=r;
+     for (size_t i=0; i < lhs.getN(); i++ ) {
+        (res)[i]= (l).getPtrArr()[i]+(r).getPtrArr()[i];
+        //r[i]=lhs[i]+rhs[i];
+        }
+     return (res);
+     }
+    else 
+    {
+     cout << "Вектора должны быть одинаковой длины!\n"; 
+     throw -1;
+    }
+}
+//поменять? это не похоже на правду... мняются исходники либо отбрасывается квалификатор...
+CVektor& operator-(const CVektor &lhs, const CVektor &rhs) 
+{
+ if(lhs.getN()==rhs.getN())
+     {
+     const CVektor& tmpl=lhs; const CVektor& tmpr=rhs;
+     CVektor& l=const_cast<CVektor&>(tmpl);//=const_cast<CVektor&>(w);
+     CVektor& r=const_cast<CVektor&>(tmpr);
+     CVektor& res=r;
+     for (size_t i=0; i < lhs.getN(); i++ ) {
+        (res)[i]= (l).getPtrArr()[i]+(r).getPtrArr()[i];
+        }
+     return (res);
+     }
+    else 
+    {
+     cout << "Вектора должны быть одинаковой длины!\n"; 
+     throw -1;
+    }
+} 
+/*CVektor0& operator+(const CVektor0 &lhs, const CVektor0 &rhs)
+{
+ if(lhs.getN()==rhs.getN())
+     {
+     //const CVektor& w=rhs; const CVektor& u=lhs; 
+     const CVektor0& tmpl=lhs; const CVektor0& tmpr=rhs;
+     CVektor0& l=const_cast<CVektor0&>(tmpl);//=const_cast<CVektor&>(w);
+     CVektor0& r=const_cast<CVektor0&>(tmpr);
+     CVektor0& res=r;
+     for (size_t i=0; i < lhs.getN(); i++ ) {
+        (res)[i]= (l).getPtrArr()[i]+(r).getPtrArr()[i];
+        //r[i]=lhs[i]+rhs[i];
+        }
+     return (res);
+     }
+    else 
+    {
+     cout << "Вектора должны быть одинаковой длины!\n"; 
+     throw -1;
+    }
+}
+//поменять
+CVektor0& operator-(const CVektor0 &lhs, const CVektor0 &rhs) 
+{
+ if(lhs.getN()==rhs.getN())
+     {
+     const CVektor0& tmpl=lhs; const CVektor0& tmpr=rhs;
+     CVektor0& l=const_cast<CVektor0&>(tmpl);//=const_cast<CVektor&>(w);
+     CVektor0& r=const_cast<CVektor0&>(tmpr);
+     CVektor0& res=r;
+     for (size_t i=0; i < lhs.getN(); i++ ) {
+        (res)[i]= (l).getPtrArr()[i]+(r).getPtrArr()[i];
+        }
+     return (res);
+     }
+    else 
+    {
+     cout << "Вектора должны быть одинаковой длины!\n"; 
+     throw -1;
+    }
+} */
+CVektor1& operator+(const CVektor1 &lhs, const CVektor1 &rhs)
+{
+ if(lhs.getN()==rhs.getN())
+     {
+      const CVektor1& tmpl=lhs; const CVektor1& tmpr=rhs;
+     CVektor1& l=const_cast<CVektor1&>(tmpl);//=const_cast<CVektor&>(w);
+     CVektor1& r=const_cast<CVektor1&>(tmpr);
+     CVektor1& res=r;
+     for (size_t i=0; i < lhs.getN(); i++ ) {
+        (res)[i]= (l).getPtrArr()[i]+(r).getPtrArr()[i];
+        }
+     return (res);
+     /*const CVektor1& res=rhs;
+     for (size_t i=0; i < lhs.getN(); i++ ) {
+        const_cast<CVektor1&>(res)[i]= const_cast<CVektor1&>(lhs).getPtrArr()[i]+const_cast<CVektor1&>(rhs).getPtrArr()[i];
+        //r[i]=lhs[i]+rhs[i];
+        }
+     return const_cast<CVektor1&>(res);*/
+     }
+    else 
+    {
+     cout << "Вектора должны быть одинаковой длины!\n"; 
+     throw -1;
+    }
+}
+CVektor1& operator-(const CVektor1 &lhs, const CVektor1 &rhs)
+{
+ if(lhs.getN()==rhs.getN())
+     {
+      const CVektor1& tmpl=lhs; const CVektor1& tmpr=rhs;
+     CVektor1& l=const_cast<CVektor1&>(tmpl);//=const_cast<CVektor&>(w);
+     CVektor1& r=const_cast<CVektor1&>(tmpr);
+     CVektor1& res=l;
+     for (size_t i=0; i < lhs.getN(); i++ ) {
+        (res)[i]= (l).getPtrArr()[i]-(r).getPtrArr()[i];
+        }
+     return (res);
+     }
+    else 
+    {
+     cout << "Вектора должны быть одинаковой длины!\n"; 
+     throw -1;
+    }
+}
+/*CVektor& CVektor::operator=(const CVektor&& v)
+    {
+        if(this!=&&v) 
+        {Clean(); CopyOnly(v);}
+        return *this;
+    }*/
+/*CVektor& CVektor::operator+(const CVektor &v) 
+    {
+     CVektor& r=const_cast<CVektor&>(v);r.SetZero();
+     if(n==v.n)
+     {
+     for (size_t i=0; i < n; i++ ) {
+        r[i]= v.ptrArr[i]+ptrArr[i];
+        }
+     return r;
+     }
+    else 
+    {
+     cout << "Вектора должны быть одинаковой длины!\n"; 
+     throw -1;
+    }
+    }
+CVektor1& CVektor1::operator+(const CVektor1 &v) 
+    {
+     //size_t m=v.n; //CVektor1 const_cast<CVektor1&>(w)=v;
+     CVektor1& r=const_cast<CVektor1&>(v);//r.SetZero();
+     if(n==v.n)
+     {for (size_t i=0; i < n; i++ ) { r[i]= v.ptrArr[i]+ptrArr[i];} return r;}
+     else {cout << "n="<<n<<"; v.n="<<v.n<<"; Вектора должны быть одинаковой длины!\n"; throw -1;}
+    }*//*
+CVektor& CVektor::operator-(const CVektor &v) 
+    {
+     CVektor& r=const_cast<CVektor&>(v);r.SetZero();
+      if(n==v.n)
+     {
+     for (size_t i=0; i < n; i++ ) {
+        r[i]= ptrArr[i]-v.ptrArr[i];
+        }
+     return r;
+     }
+    else 
+    {
+     cout << "Вектора должны быть одинаковой длины!\n"; 
+     throw -1;
+    }
+    }*/
+/*CVektor1& CVektor1::operator-(const CVektor1 &v) 
+    {
+     //size_t m=v.n; //CVektor1 const_cast<CVektor1&>(w)=v;
+     const CVektor1 &w=v;
+     CVektor1& r=const_cast<CVektor1&>(v);//r.SetZero();
+     if(n==v.n)
+     {for (size_t i=0; i < n; i++ ) { r.setPos(i, ptrArr[i]-v.ptrArr[i]);} 
+     const_cast<CVektor1&>(v)=const_cast<CVektor1&>(w);return r;}
+     else {cout << "n="<<n<<"; v.n="<<v.n<<"; Вектора должны быть одинаковой длины!\n"; throw -1;}
+    }*/
+float CVektor::operator *(const CVektor &v)
+    {
+      float r=0;
+     //n=v.n;
+     if(n==v.n)
+     {
+     for (size_t i=0; i < v.n; i++ ) {
+        r += v.ptrArr[i]*ptrArr[i];
+        }
+     return r;
+     }
+    else 
+    {
+     cout << "Вектора должны быть одинаковой длины!\n"; 
+     throw -1;
+    }
+    }
+
+//////////////////FRIEND FUNCTIONS////////////////////////
+
+ostream &operator<<(ostream& cout, CVektor &v) 
+{
+   cout << "Вектор {";
+     for (size_t i=0; i < v.n; i++)
+     { 
+     if(i<v.getN()-1)
+ //        cout << v.getPtrArr()[i]  << ", ";
+       cout << v.ptrArr[i]  << ", ";
+     else //if(i == v.n-1)
+ //        cout << v.getPtrArr()[i]; 
+        cout << v.ptrArr[i] ;
+     }    
+   cout << "}\n";
+
+return cout;
+}
+istream &operator>>(istream& cin , CVektor &v) 
+{
+ for (size_t i=0,p; i < v.getN(); i++ )
+        {
+            cout << "Введите элемент вектора № " << i << ": ";
+            cin >> p;      //v.ptrArr[i];
+            v.ptrArr[i]=p;
+        }
+return cin;
+}
---
+++ CVektor.h
@@ -0,0 +1,129 @@
+/*
+ * CVector.h
+ * 8. Определить класс CVector для работы с вектором вещественных
+ * чисел. Длина вектора задается в конструкторе класса. В классе
+ * должны быть определены необходимые конструкторы, деструктор, операторы сло-
+ * жения, вычитания, скалярного умножения.
+ * Написать функцию и конструктор сохранения переменной данного
+ * типа в файл и загрузки из файла.
+ * В отдельном файле должен быть написан тест на данный класс.
+ *     
+ */
+#ifndef Vektor_H
+#define Vektor_H
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <cstdlib>
+using namespace std;
+//--------------------------------------------------------------
+//----------- Определение класса -------------------------------
+//--------------------------------------------------------------
+/*
+TODO:  move constructor
+DONE: operator +-; ГДЕ ОПЕДЕЛИТЬ ГЛОБАЛЬНУЮ ПЕРЕМЕННУЮ?, const & in Kommutativ/Netativ; define Clear, SetZero, CopyOnly; changed operator =; dealed with uninitialized vars; dealed with mismatched free/delete [BY NOT USING SETN() AND REALLOC, IT'S TOO BAD]; did normal default constructor; 2 tests; operator[]; deal w operator =; КАК ПЕРЕЙТИ НА СЛЕДУЮЩУЮ СТРОКУ finput?; что не так с передачей glob_v? ; copy constructor for children; 
+*/
+class CVektor
+{
+protected:
+    size_t n;                 //Размер вектора
+    float *ptrArr;     //Указатель на массив элементов
+    //char* OutFile;
+public:
+    CVektor (const CVektor&v) {CopyOnly(v);}
+    CVektor()      { SetZero(); /*n=0; ptrArr = new float[n];*/ /*ptrArr = (float*)realloc(ptrArr, n*sizeof(float));*/ }
+    CVektor(size_t num)   { n = num;   ptrArr = new float [num]; memset(ptrArr,0,n*sizeof(float)); }
+    virtual ~CVektor()    { Clean(); }
+    
+    CVektor ( CVektor && ) = default; 	//move constructor
+    //CVektor&  operator= ( CVektor&& );  //move assignment
+    size_t getN() const{ return n; }                      //getter
+    //void setN(int dimension) { n = dimension; ptrArr =(float*)realloc(ptrArr, n*sizeof(float));}   //setter
+    float* getPtrArr() const{ return ptrArr; }
+    void SetZero(){ptrArr=NULL;n=0;}
+    void Clean() {if(ptrArr!=NULL)delete [] ptrArr; SetZero();}
+    void setPtrArr(float* arr)  { for (size_t i=0; i < n; i++ ) {ptrArr[i] = arr[i]; } } 
+    void GetVektor();
+    void ShowVektor();
+    void CopyOnly(const CVektor &v);
+    void setPos(size_t i, float f) {if(/*i<0||*/i>n-1) throw -1; 
+    //if(!ptrArr){ptrArr = new float[i];memset(ptrArr,0,i*sizeof(float)); }
+    ptrArr[i]=f;}
+    /*const*/ CVektor& operator=(const CVektor& v);
+    //CVektor &operator=( CVektor0 &b);
+    /*const */ //CVektor& operator+(const CVektor &v);
+    /*const  CVektor& operator-(const CVektor &v);*/
+    float operator *(const CVektor &v);
+    float &operator[](size_t i){if(/*i<0||*/i>n-1) throw -1; return (this->ptrArr)[i];}
+    //float &operator[](int i){if(i<0||i>n-1) throw -1; return ptrArr[i];}
+
+    virtual int output(const char *FileName=NULL)=0;
+    virtual const char* getF()const=0;
+    virtual void setText(char* c)=0;
+    static CVektor **Input(const char *fn);
+    
+    friend float Skalar   (CVektor& , CVektor&);  //Склярное произведение
+ 
+    friend CVektor& Kommutativ(const CVektor& , const CVektor& );//Сложение векторов
+    friend CVektor& Netativ   (const CVektor& , const CVektor& );//Вычитание векторов
+    friend int NReader();
+    friend ostream &operator<<(ostream& /*&cout*/, CVektor& );
+    friend istream &operator>>(istream& /*&cin*/, CVektor& ); 
+};
+
+CVektor& operator+(const CVektor &lhs, const CVektor &rhs);
+CVektor& operator-(const CVektor &lhs, const CVektor &rhs);
+
+class CVektor0: public CVektor {
+   private:
+      char OutFile[15];
+   public:
+      CVektor0 (const CVektor0 &v):CVektor(v){if(this!=&v){ strncpy(OutFile,v.OutFile,15);}}
+      CVektor0() {ptrArr=NULL;n=0;}
+      CVektor0(int num) : CVektor(num){memset(OutFile,0,15);/* n = num;   ptrArr = new float [num]; memset(ptrArr,0,n*sizeof(float));*/}
+      //CVektor0(int num)   { n = num;   ptrArr = new float [num];  }
+      ~CVektor0()    {delete [] ptrArr; ptrArr=NULL;n=0;}
+      void CopyOnly(const CVektor0 &v) {if(this!=&v){memcpy(ptrArr=new float[n=v.getN()], v.ptrArr, v.getN()*sizeof(float)); strncpy(OutFile,v.getF(),15);}}
+      //~CVektor0() : ~CVektor() {}
+      CVektor0 &operator=( CVektor0&b){if(this!=&b){memcpy(ptrArr,b.getPtrArr(),b.getN()*sizeof(float)); n=b.getN(); strcpy(OutFile,b.getF());} return *this;}
+      CVektor0 &operator=( CVektor &b){if(this!=&b){memcpy(ptrArr,b.getPtrArr(),b.getN()*sizeof(float)); n=b.getN();} return *this;}
+      CVektor0 &operator+( CVektor &v){if(n==v.getN()){for (size_t i=0; i < n; i++ ) {ptrArr[i]= v.getPtrArr()[i]+ptrArr[i];} return *this;} else {cout << "Вектора должны быть одинаковой длины!\n"; throw -1;}}
+      CVektor0 &operator-( CVektor &v){if(n==v.getN()){for (size_t i=0; i < n; i++ ) {ptrArr[i]= ptrArr[i]-v.getPtrArr()[i];} return *this;} else {cout << "Вектора должны быть одинаковой длины!\n"; throw -1;}}
+      void SetZero(){ptrArr=NULL;n=0;}
+      void Clean() {delete [] ptrArr; SetZero();}
+      const char* getF() const{return OutFile;}
+      void setText(char* c)  { strcpy(OutFile,c); } 
+      int output(const char *FileName); //в строку
+};
+
+//иначе имеем ambiguating
+//CVektor0& operator+(const CVektor0 &lhs, const CVektor0 &rhs);
+//CVektor0& operator-(const CVektor0 &lhs, const CVektor0 &rhs);
+
+class CVektor1: public CVektor {
+   private:
+      char OutFile[15];
+   public:
+      CVektor1 (const CVektor1 &v):CVektor(v){if(this!=&v){ strncpy(OutFile,v.OutFile,15);}}
+      CVektor1() {ptrArr=NULL;n=0;}
+      CVektor1(int num) : CVektor(num){memset(OutFile,0,15);/* n = num;   ptrArr = new float [num]; memset(ptrArr,0,n*sizeof(float));*/}
+      //CVektor0(int num)   { n = num;   ptrArr = new float [num];  }
+      ~CVektor1()    {delete [] ptrArr; ptrArr=NULL;n=0;}
+      //~CVektor0() : ~CVektor() {}
+      CVektor1 &operator=( CVektor1&b){if(this!=&b){memcpy(ptrArr,b.getPtrArr(),b.getN()*sizeof(float)); n=b.getN();} return *this;}
+      CVektor1 &operator=( CVektor&b){if(this!=&b){memcpy(ptrArr,b.getPtrArr(),b.getN()*sizeof(float)); n=b.getN();} return *this;}
+      //CVektor1& operator+(const CVektor1 &v);
+      //{if(n==v.getN()){CVektor1 w(n);for (size_t i=0; i < n; i++ ) {w.setPos(i,ptrArr[i]+v.getPtrArr()[i]);} *this=w;return *this;} else {cout << "Вектора должны быть одинаковой длины!\n"; throw -1;}}
+      //CVektor1& operator-(const CVektor1 &v);//{if(n==v.getN()){CVektor1 w(n);for (size_t i=0; i < n; i++ ) {w.setPos(i,ptrArr[i]-v.getPtrArr()[i]);} *this=w;return *this;} else {cout << "Вектора должны быть одинаковой длины!\n"; throw -1;}}
+      void SetZero(){ptrArr=NULL;n=0;}
+      void Clean() {delete [] ptrArr; SetZero();}
+      const char* getF() const{return OutFile;}
+      void setText(char* c)  { strcpy(OutFile,c); } 
+      int output(const char *FileName); //в столбец
+};
+#endif 
+//иначе имеем ambiguating
+CVektor1& operator+(const CVektor1 &lhs, const CVektor1 &rhs);
+CVektor1& operator-(const CVektor1 &lhs, const CVektor1 &rhs);
+//extern CVektor **glob_v; 
+extern size_t glob_n;
